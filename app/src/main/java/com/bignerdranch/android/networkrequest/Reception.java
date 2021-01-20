729466904db283d0dcb6fbda2c411b0438685660
xiaozhu-sorce <790362227@qq.com>
2021-01-20 17:48:42 +0800

    youdao

--- app/src/main/java/com/bignerdranch/android/networkrequest/Reception.java
+++ app/src/main/java/com/bignerdranch/android/networkrequest/Reception.java
@@ -2,6 +2,10 @@ package com.bignerdranch.android.networkrequest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,12 +16,25 @@ import retrofit2.converter.gson.GsonConverterFactory;
 //创建接受服务器返回数据的类
 public class Reception extends AppCompatActivity {
 
+    private Button mButton;
+
+    private TextView mTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        request();
+        mButton=(Button)findViewById(R.id.btn_get);
+        mTextView=(TextView)findViewById(R.id.text);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request();
+            }
+        });
     }
 
     public void request(){
@@ -32,14 +49,15 @@ public class Reception extends AppCompatActivity {
             //请求成功时回调
             @Override
             public void onResponse(Call<Translation> call, Response<Translation> response) {
-                // 步骤7：处理返回的数据结果
-                System.out.println(response.body().getTranslateResult().get(0).get(0).getTgt());
+                // 步骤7：
+                Toast.makeText(Reception.this,"get回调成功：异步执行",Toast.LENGTH_SHORT).show();
+                mTextView.setText(response.body().getTranslateResult().get(0).get(0).getTgt());
             }
 
             //请求失败时回调
             @Override
             public void onFailure(Call<Translation> call, Throwable throwable) {
-                System.out.println("连接失败");
+                mTextView.setText("连接失败");
                 System.out.println(throwable.getMessage());
             }
         });
